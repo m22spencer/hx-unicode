a546480bbce9adc2896cdf6b768a24d2671daece
Matthew Spencer <m22spencer@gmail.com>
2017-05-02 09:55:13 -0400

    Remove UnicodeString

--- src/unicode/Unicode.hx
+++ src/unicode/Unicode.hx
@@ -1,6 +1,5 @@
 package unicode;
 
-typedef UnicodeString  = unicode.tr29.Unicode.UnicodeString;
 typedef Characters     = unicode.tr29.Unicode.Characters;
 typedef Character      = unicode.tr29.Unicode.Character;
 typedef UnicodeScalars = unicode.tr29.Unicode.UnicodeScalars;
--- src/unicode/tr29/Unicode.hx
+++ src/unicode/tr29/Unicode.hx
@@ -2,26 +2,16 @@ package unicode.tr29;
 
 import unicode.tr29.internal.Helper;
 
-abstract UnicodeString(String) from String {
-    public static function unicodeString(str:String):UnicodeString {
-        return str;
-    }
-
-    public static function characters(str:haxe.extern.EitherType<UnicodeString,String>):Characters {
-        return cast(str,String);
-    }
-
-    public static function scalars(str:haxe.extern.EitherType<UnicodeString,String>):UnicodeScalars {
-        return cast(str,String);
-    }
-}
-
 abstract Characters(String) from String {
     public function count():UInt return Helper.unicodeCharacterCount(this);
     public function toString():String return this;
 
     public function substr(pos:Int, ?len:Int) {
         return untyped Helper.unicodeCharacterSubstr(this, pos, len);
+    }
+
+    public static function scalars(str:haxe.extern.EitherType<Characters,String>):UnicodeScalars {
+        return cast(str,String);
     }
 
     public function iterator():Iterator<Character> {
@@ -42,6 +32,10 @@ abstract UnicodeScalars(String) from String {
     @:allow(unicode.UnicodeString)
     function new(str:String) this = str;
     public function toString() return this;
+
+    public static function characters(str:haxe.extern.EitherType<UnicodeScalars,String>):Characters {
+        return cast(str,String);
+    }
 
     public function iterator():Iterator<UnicodeScalar> {
         return untyped (Helper.unicodeScalars(this).iterator());
